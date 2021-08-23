565aac1ad838f6cc932b4bdd2ce1020291fdbc97
soum25 <81410961+soum25@users.noreply.github.com>
2021-08-23 10:59:56 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-from articles_app.api.routes.articles import articles_router
-
-app = Flask(__name__)
-CORS(app)
-app.register_blueprint(articles_router)
-
-app.run()
